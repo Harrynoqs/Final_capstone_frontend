ae88655cea7b2ab98a940b09b290ceace7ac3f97
binyamyohannes <olangobinyam@gmail.com>
2024-02-19 17:23:41 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Item from './components/Item/Item';
+import AddItem from './components/AddItem/AddItem';
+import DeleteItem from './components/DeleteItem/DeleteItem';
+import AddReservation from './components/AddReservation/AddReservation';
+import MyReservation from './components/MyReservation/MyReservation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Item />} />
+          <Route path="/adddoctor" element={<AddItem />} />
+          <Route path="/deletedoctor" element={<DeleteItem />} />
+          <Route path="/bookappointment" element={<AddReservation />} />
+          <Route path="/myappointments" element={<MyReservation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
