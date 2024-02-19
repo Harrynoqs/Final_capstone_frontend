1598e507127045ec32b108a466f933d09752f580
binyamyohannes <olangobinyam@gmail.com>
2024-02-19 17:22:53 +0300

    Add myreservation component

---
+++ src/components/MyReservation/MyReservation.js
@@ -0,0 +1,10 @@
+import Navigation from '../Navigation/Navigation';
+
+const MyReservation = () => (
+  <>
+    <Navigation />
+    <h1>My Reservations</h1>
+  </>
+);
+
+export default MyReservation;
