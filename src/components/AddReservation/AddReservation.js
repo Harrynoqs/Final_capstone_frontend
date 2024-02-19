57e3d8fb8ea07aa5fb9b4d5690834ec35546a6c9
binyamyohannes <olangobinyam@gmail.com>
2024-02-19 17:21:02 +0300

    Add addreservation component

---
+++ src/components/AddReservation/AddReservation.js
@@ -0,0 +1,10 @@
+import Navigation from '../Navigation/Navigation';
+
+const AddReservation = () => (
+  <>
+    <Navigation />
+    <h1>Reserve Form</h1>
+  </>
+);
+
+export default AddReservation;
